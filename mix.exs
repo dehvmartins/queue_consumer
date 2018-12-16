450c49f13d81b80715b536ba24c2bca4be9905a3
Nico <nico.piderman@gmail.com>
2018-12-16 10:55:13 -0500

    relax elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule QueueConsumer.MixProject do
     [
       app: :queue_consumer,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps(),
